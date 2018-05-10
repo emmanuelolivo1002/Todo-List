aedce8b30180c33c520d916e4f24b053510e0210
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-10 12:43:32 -0500

    Add todo items with input

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,24 @@
 // Check off specific items by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("checked-off");
 });
 
 //Click on X to delete TO-DO
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   $(this).parent().fadeOut(500, function () {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+//Add new item
+$("input[type='text']").keypress(function functionName(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    if (todoText != "") {
+      $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+  }
+
 });
